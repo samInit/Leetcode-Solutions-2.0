1e13330c699c9927b6733b049425e9dcdcdcfd9d
Sam <sameeramadhu9304@gmail.com>
2025-11-14 13:59:16 +0530

    Time: 659 ms (5.26%) | Memory: 77.3 MB (11.84%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
         int[][] arr = new int[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j<n; j++){
-                arr[i][j] = 0;
-            }
-        }
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j<n; j++){
+        //         arr[i][j] = 0;
+        //     }
+        // }
 
         for(int[] query: queries){
             for(int row1 = query[0]; row1 <= query[2]; row1++){
